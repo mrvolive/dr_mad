bca098e6a9b7404d6c1ce69d3c55b4dccf767882
ImmerAlex <aleximm67@gmail.com>
2024-11-21 15:34:10 +0100

    add LeftCenterRightViews

---
+++ src/components/views/LeftCenterRightViews.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="d-flex justify-content-between gap-5">
+    <router-view name="left" style="width: 20dvw; padding: 0 0 0 10px;"/>
+    <router-view name="center" style="width: 60dvw;"/>
+    <router-view name="right" style="width: 20dvw; padding: 0 10px 0 0;"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LeftCenterRightViews',
+}
+</script>
